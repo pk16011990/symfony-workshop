33e128852848ccc6cdc4901de04447f2f09c9bed
Petr Kadlec <petr.kadlec@shopsys.com>
2019-04-22 15:33:54 +0200

    route definition as a part of controller action

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -5,9 +5,13 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class HomepageController
 {
+    /**
+     * @Route(name="homepage", path="/")
+     */
     public function homepage(Request $request): Response
     {
         return new Response('Hello ' . $request->get('name', 'nobody'));
