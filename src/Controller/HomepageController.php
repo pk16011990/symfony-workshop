22cbd0baa9e140d1192942462ef783243b6c5c4e
Petr Kadlec <petr.kadlec@shopsys.com>
2019-04-22 15:37:23 +0200

    parameter in route

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -3,7 +3,6 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,8 +11,16 @@ class HomepageController
     /**
      * @Route(name="homepage", path="/")
      */
-    public function homepage(Request $request): Response
+    public function homepage(): Response
     {
-        return new Response('Hello ' . $request->get('name', 'nobody'));
+        return new Response('Hello all');
+    }
+
+    /**
+     * @Route(name="hello", path="/hello/{name}")
+     */
+    public function hello(string $name = 'nobody'): Response
+    {
+        return new Response('Hello ' . $name);
     }
 }
