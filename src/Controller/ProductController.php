e0497661a4f435e8d81074eb2c7b064d551488ef
Petr Kadlec <petr.kadlec@shopsys.com>
2019-04-22 16:31:21 +0200

    handling of submitted form

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 
 use App\Model\Product\ProductForm;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,11 +13,19 @@ class ProductController extends AbstractController
 {
     /**
      * @Route("product/form")
+     * @param \Symfony\Component\HttpFoundation\Request $request
      * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function form(): Response
+    public function form(Request $request): Response
     {
         $form = $this->createForm(ProductForm::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            dump($form->getData());
+
+            return $this->redirectToRoute('homepage');
+        }
 
         return $this->render('Product/form.html.twig', [
             'formView' => $form->createView(),
